3e2037efb883061cad8254fea134356e700bf55c
Joel <ramos.joeled@gmail.com>
2020-09-10 19:37:22 +0000

    finished this exercise

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const value1 = Math.floor(Math.random() * 100);
-const value2 = Math.floor(Math.random() * 100);
-const value3 = Math.floor(Math.random() * 100);
-const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-const numQuestions = 0;
-const numCorrect = 0;
+import Score from './Score.js';
+import Game from './Game.js';
 
 class App extends Component {
+  
+  state = {
+  	numQuestions: 0,
+    numCorrect: 0
+  }
+
+handleAnswer = answerWasCorrect => {
+    if (answerWasCorrect) {
+      this.setState(currState => ({
+        numCorrect: currState.numCorrect + 1,
+      }));
+    }
+    this.setState(currState => ({
+      numQuestions: currState.numQuestions + 1,
+    }));
+  };
+  
   render() {
     return (
       <div className="App">
@@ -19,14 +32,8 @@ class App extends Component {
         </header>
         <div className="game">
           <h2>Mental Math</h2>
-          <div className="equation">
-            <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
-          </div>
-          <button>True</button>
-          <button>False</button>
-          <p className="text">
-            Your Score: {numCorrect}/{numQuestions}
-          </p>
+          <Game handleAnswer={this.handleAnswer} />
+          <Score numCorrect={this.state.numCorrect} numQuestions={this.state.numQuestions} />
         </div>
       </div>
     );
---
+++ src/Score.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Score(props) {
+	return (
+      <p className="text">Your Score: {props.numCorrect}/{props.numQuestions}</p>
+    )
+}
+
+export default Score;
